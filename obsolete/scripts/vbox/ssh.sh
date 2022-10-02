f55efb4fa8b4bc4b2d7960bcb0be2c1520ac70de
Andreia Ocanoaia <andreia.ocanoaia@gmail.com>
2022-10-02 10:48:00 +0300

    Clean old files that are not needed.
    Signed-off-by: Andreia Ocanoaia <andreia.ocanoaia@gmail.com>

--- obsolete/scripts/vbox/ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p /home/student/.ssh
-mv /home/student/authorized_keys /home/student/.ssh
-chown -R student:student /home/student/.ssh
-echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-service ssh restart
